55581e8072297cacf273e8e18b93c1091e968c86
VoQuocTrung <votrung2202@gmail.com>
2025-03-30 09:44:21 +0700

    Test CI/CD pipeline

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, My name is Trung!"
+    return """
+    <div style="font-family: Arial, sans-serif; text-align: center; margin-top: 20%; background-color: #e6f3ff; padding: 20px; border-radius: 10px; width: 300px; margin-left: auto; margin-right: auto;">
+        <h1 style="color: #2c3e50;">Xin chào!</h1>
+        <p style="color: #7f8c8d; font-size: 18px;">Mình là Trung!</p>
+    </div>
+    """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
