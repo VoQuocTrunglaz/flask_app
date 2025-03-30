5d85832017f44bee6ca779869de839467f673f66
VoQuocTrung <votrung2202@gmail.com>
2025-03-30 13:32:10 +0700

    Update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def home():
     return """
     <div style="font-family: Arial, sans-serif; text-align: center; margin-top: 20%; background-color: #e6f3ff; padding: 20px; border-radius: 10px; width: 300px; margin-left: auto; margin-right: auto;">
         <h1 style="color: #2c3e50;">Xin chào!</h1>
-        <p style="color: #7f8c8d; font-size: 18px;">Chào mừng bạn đến với AWS!</p>
+        <p style="color: #7f8c8d; font-size: 18px;">Chào mừng bạn đến với Flask API!</p>
     </div>
     """
 
